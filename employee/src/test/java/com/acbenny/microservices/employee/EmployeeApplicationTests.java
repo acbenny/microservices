6bc9851d24d3bf1145c002de374bc7e5d09626ea
Arun Benny <acbenny@gmail.com>
2020-02-05 21:30:24 +0000

    Adding employee service

---
+++ employee/src/test/java/com/acbenny/microservices/employee/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acbenny.microservices.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
