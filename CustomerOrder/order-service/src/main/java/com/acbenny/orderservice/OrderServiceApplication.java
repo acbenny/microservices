aa7422fb92b2c50b0905e9b7a8ace7fe4b9f4179
Arun Benny <acbenny@outlook.com>
2020-03-05 16:47:28 +0000

    OrderService Main Class cleanup

--- CustomerOrder/order-service/src/main/java/com/acbenny/orderservice/OrderServiceApplication.java
+++ CustomerOrder/order-service/src/main/java/com/acbenny/orderservice/OrderServiceApplication.java
@@ -2,48 +2,11 @@ package com.acbenny.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.MapPropertySource;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 @SpringBootApplication
 public class OrderServiceApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
-    }
-
-    @EventListener
-    public void WebServerInitializedHandler(ContextRefreshedEvent event) {
-        ConfigurableEnvironment evn = (ConfigurableEnvironment) event.getApplicationContext().getEnvironment();
-
-        System.out.println("************************* ACTIVE APP PROPERTIES ******************************");
-
-        List<MapPropertySource> propertySources = new ArrayList<>();
-
-        evn.getPropertySources().forEach(it -> {
-            if (it instanceof MapPropertySource && it.getName().contains("applicationConfig")) {
-                propertySources.add((MapPropertySource) it);
-            }
-        });
-
-        propertySources.stream()
-                .map(propertySource -> propertySource.getSource().keySet())
-                .flatMap(Collection::stream)
-                .distinct()
-                .sorted()
-                .forEach(key -> {
-                    try {
-                        System.out.println(key + "=" + evn.getProperty(key));
-                    } catch (Exception e) {
-                        System.out.println(key + e.getMessage());
-                    }
-                });
-        System.out.println("******************************************************************************");
     }
 }
